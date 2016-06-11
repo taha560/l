6e930c2dceb3773fdcff63d163442dfe7ad92372
taha560 <taha102050@gmail.com>
2016-06-11 03:24:15 -0700

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,6 +1,6 @@
 do
     local function run(msg, matches)
-    local support = '138776903' -- آیدی ساپورت بات رو اینجا قرار دهید
+    local support = '189308877' -- آیدی ساپورت بات رو اینجا قرار دهید
     local data = load_data(_config.moderation.data)
     local name_log = user_print_name(msg.from)
         if matches[1] == 'support' then
